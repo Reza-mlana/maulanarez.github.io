19f3a2c88cc28c15de00dbd8b6f15e2760ecd578
Reza-mlana <rezabae0911@gmail.com>
2021-07-17 13:26:35 +0700

    first commit

---
+++ peta/data/wisata_1.js
@@ -0,0 +1,19 @@
+var json_wisata_1 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "45546", "wisata": "pantai widuri" }, "geometry": { "type": "Point", "coordinates": [ 109.37780718731243, -6.858966145922559 ] } },
+{ "type": "Feature", "properties": { "id": "6534", "wisata": "curug sibedil" }, "geometry": { "type": "Point", "coordinates": [ 109.23255904695024, -7.127845783848952 ] } },
+{ "type": "Feature", "properties": { "id": "35346", "wisata": "bukit tangkeban" }, "geometry": { "type": "Point", "coordinates": [ 109.23286095536919, -7.160794970845524 ] } },
+{ "type": "Feature", "properties": { "id": "423", "wisata": "curug bengkawah" }, "geometry": { "type": "Point", "coordinates": [ 109.32794838423187, -7.117350349357077 ] } },
+{ "type": "Feature", "properties": { "id": "2343435", "wisata": "telaga jendul" }, "geometry": { "type": "Point", "coordinates": [ 109.44220150845459, -7.137416280730002 ] } },
+{ "type": "Feature", "properties": { "id": "2324", "wisata": "taman senja kwasen" }, "geometry": { "type": "Point", "coordinates": [ 109.48296463425821, -7.043023241230936 ] } },
+{ "type": "Feature", "properties": { "id": "4534", "wisata": "air babakan" }, "geometry": { "type": "Point", "coordinates": [ 109.31140380287272, -7.08810091280376 ] } },
+{ "type": "Feature", "properties": { "id": "35345", "wisata": "pantai joko tingkir" }, "geometry": { "type": "Point", "coordinates": [ 109.45704305186838, -6.830979235484866 ] } },
+{ "type": "Feature", "properties": { "id": "1213", "wisata": "pantai muara indah" }, "geometry": { "type": "Point", "coordinates": [ 109.41275583142874, -6.850072884517637 ] } },
+{ "type": "Feature", "properties": { "id": "4535", "wisata": "hutan mangrove" }, "geometry": { "type": "Point", "coordinates": [ 109.52126720644722, -6.780732754548333 ] } },
+{ "type": "Feature", "properties": { "id": "323242", "wisata": "bendungan kali lumeneng" }, "geometry": { "type": "Point", "coordinates": [ 109.41593959293775, -7.107825824894356 ] } },
+{ "type": "Feature", "properties": { "id": "223323", "wisata": "dermaga susur kali comal" }, "geometry": { "type": "Point", "coordinates": [ 109.47278620360599, -6.961548451285889 ] } },
+{ "type": "Feature", "properties": { "id": "124335", "wisata": "taman kota comal" }, "geometry": { "type": "Point", "coordinates": [ 109.53065545858962, -6.906122288861018 ] } }
+]
+}
